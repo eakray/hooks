59984e4b1d6e533d93c104bbbd3229f8c0f6adc2
Eugene Krayni <ekrayni@gmail.com>
2022-11-14 15:51:51 -0600

    pust useState in react namespace

--- hooks.js
+++ hooks.js
@@ -1,13 +1,18 @@
-function useState(initVal) {
-    let _val = initVal;
-    let state = initVal;
-    let setState = (newVal) => {
-        _val = newVal;
-    };
-    return [state, setState];
-}
+// namespace useState in a react object, module pattern
 
-const [count, setCount] = useState(1);
-console.log(count);
+const React = (() => {
+    function useState(initVal) {
+        let _val = initVal;
+        let state = () => _val;
+        let setState = (newVal) => {
+            _val = newVal;
+        };
+        return [state, setState];
+    }
+    return { useState };
+})();
+
+const [count, setCount] = React.useState(1);
+console.log(count());
 setCount(2);
-console.log(count);
+console.log(count());
